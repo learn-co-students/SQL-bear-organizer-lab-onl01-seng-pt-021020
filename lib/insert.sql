842cdfc9415deded12babdd57d1def29649d24d7
MKNJ1961 <skillcrush1961@gmail.com>
2020-03-30 06:25:09 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, color)VALUES('Mr Chocolate', 2, 'Black');
+INSERT INTO bears (name, age, color)VALUES('Rowdy', 4, 'Brown');
+INSERT INTO bears (name, age, color)VALUES('Tabitha', 6, 'Black');
+INSERT INTO bears (name, age, color)VALUES('Sergeant Brown', 8, 'BRrown');
+INSERT INTO bears (name, age, color)VALUES('Melissa', 8, 'Black');
+INSERT INTO bears (name, age, color)VALUES('Grinch', 4, 'Black');
+INSERT INTO bears (name, age, color)VALUES('Wendy', 7, 'Brown');
+INSERT INTO bears (name, age, color)VALUES(NULL, NULL, 'Black');
