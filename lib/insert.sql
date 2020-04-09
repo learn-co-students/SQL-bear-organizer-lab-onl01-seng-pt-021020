544f7481daee822d7fd072375c091e59d98d98e9
Amber Williams <awilliams1234@live.com>
2020-04-09 15:48:18 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name) VALUES ('Mr. Chocolate');
-INSERT INTO bears (name) VALUES ('Rowdy');
-INSERT INTO bears (name) VALUES ('Tabitha');
-INSERT INTO bears (name) VALUES ('Sergeant Brown');
-INSERT INTO bears (name) VALUES ('Melissa');
-INSERT INTO bears (name) VALUES ('Grinch');
-INSERT INTO bears (name) VALUES ('Wendy');
-INSERT INTO bears (name) VALUES (NULL);
+INSERT INTO bears (name, age, gender) VALUES ('Mr. Chocolate', '55', 'male');
+INSERT INTO bears (name, age, gender) VALUES ('Rowdy', '4', 'male');
+INSERT INTO bears (name, age, gender) VALUES ('Tabitha', '35', 'female');
+INSERT INTO bears (name, age, gender) VALUES ('Sergeant Brown', '67', 'male');
+INSERT INTO bears (name, age, gender) VALUES ('Melissa', '26', 'female');
+INSERT INTO bears (name, age, gender) VALUES ('Grinch', '40', 'male');
+INSERT INTO bears (name, age, gender) VALUES ('Wendy', '42', 'female');
+INSERT INTO bears (name, age, gender) VALUES (NULL, '10', 'female');
