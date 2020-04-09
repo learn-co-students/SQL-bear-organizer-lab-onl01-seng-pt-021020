bcbc780d1b659c88628407c2fbcbb167ef24aba8
Amber Williams <awilliams1234@live.com>
2020-04-09 16:04:02 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender) VALUES ('Mr. Chocolate', '55', 'male');
-INSERT INTO bears (name, age, gender) VALUES ('Rowdy', '4', 'male');
-INSERT INTO bears (name, age, gender) VALUES ('Tabitha', '35', 'female');
-INSERT INTO bears (name, age, gender) VALUES ('Sergeant Brown', '67', 'male');
-INSERT INTO bears (name, age, gender) VALUES ('Melissa', '26', 'female');
-INSERT INTO bears (name, age, gender) VALUES ('Grinch', '40', 'male');
-INSERT INTO bears (name, age, gender) VALUES ('Wendy', '42', 'female');
-INSERT INTO bears (name, age, gender) VALUES (NULL, '10', 'female');
+INSERT INTO bears (name, age, gender) VALUES ('Mr. Chocolate', '20', 'M');
+INSERT INTO bears (name, age, gender) VALUES ('Rowdy', '10', 'M');
+INSERT INTO bears (name, age, gender) VALUES ('Tabitha', '6', 'F');
+INSERT INTO bears (name, age, gender) VALUES ('Sergeant Brown', '19', 'M');
+INSERT INTO bears (name, age, gender) VALUES ('Melissa', '13', 'F');
+INSERT INTO bears (name, age, gender) VALUES ('Grinch', '2', 'M');
+INSERT INTO bears (name, age, gender) VALUES ('Wendy', '6', 'F');
+INSERT INTO bears (name, age, gender) VALUES (NULL, '20', 'M');
